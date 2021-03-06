3901541c0d9d2891ce9163d9be0f6fb595294865
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-03-06 11:43:12 +0530

    Create In Search of an Easy Problem.cpp

---
+++ In Search of an Easy Problem.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+#define lli long long int
+using namespace std;
+ 
+int main()
+{
+    lli n,x;
+    cin>>n;
+    lli q=0;
+    while(n--)
+    {
+        cin>>x;
+        if(x==1)
+        {
+            q=1;
+            break;
+        }
+    }
+    if(q==1)
+        cout<<"HARD"<<endl;
+    else
+        cout<<"EASY"<<endl;
+ 
+	return 0;
+}
