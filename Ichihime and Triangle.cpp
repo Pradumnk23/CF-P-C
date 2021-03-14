72aec71196241c114f3f6d871f80ae0057046ad0
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-03-14 19:51:29 +0530

    Print b c c dor triangle

---
+++ Ichihime and Triangle.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+#define lli long long int
+using namespace std;
+
+int main()
+{
+    lli t;
+    cin>>t;
+    while(t--)
+    {
+        lli a,b,c,d;
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+	return 0;
+}
