3656c8ef9de8057631a11244b90ac3474268879c
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-03-18 11:29:16 +0530

    N*2-N/2, :-) :-)

---
+++ Toy Army.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+#define lli long long int
+using namespace std;
+
+int main()
+{
+    lli n;
+    cin>>n;
+    cout<<n*2-n/2<<endl;
+	return 0;
+}
