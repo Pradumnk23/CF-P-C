cc46236f51735e09341e679ca04d9e4771a79ff9
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-03-22 20:20:55 +0530

    a*b

---
+++ Prison Break.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+#define lli long long int
+using namespace std;
+ 
+int main()
+{
+    lli t;
+    cin>>t;
+    while(t--)
+    {
+        lli a,b;
+        cin>>a>>b;
+        cout<<a*b<<endl;
+    }
+	return 0;
+}
