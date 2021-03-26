97d64c1d7459ec2063ad5c99031dd44074b3134f
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-03-26 21:33:48 +0530

    Print n only

---
+++ Filling Diamonds.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+#define lli long long int
+using namespace std;
+
+int main()
+{
+    lli t;
+    cin>>t;
+    while(t--)
+    {
+        lli n;
+        cin>>n;
+        cout<<n<<endl;
+    }
+    return 0;
+}
