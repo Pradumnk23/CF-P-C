f237dfc83d76aa9012aa71b5ef8d0da063cf79b1
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-04-06 20:17:25 +0530

    max=1000000000000000000, not INT_MAX

--- Hamster Farm.cpp
+++ Hamster Farm.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-    lli n,k,ans,nob,m,max=1000000000000000000;//max=INT_MAX;
+    lli n,k,ans,nob,m,max=1000000000000000000;// max=INT_MAX not working bcz of constraint
     cin>>n>>k;
     for(lli i=0; i<k; i++)
     {
@@ -23,29 +23,4 @@ int main()
     cout<<ans<<" "<<nob<<endl;
     return 0;
 }
-/*
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    unsigned long long int n;
-    long long int k;
-    cin>>n>>k;
-    unsigned long long int ans;
-    unsigned long long int hamster_remained=1000000000000000000;
-    unsigned long long int no_of_boxes;
-    unsigned long long int m;
-    for(long long int i=0;i<k;i++)
-    {
-        cin>>m;
-        if(hamster_remained>(n%m))
-        {
-            hamster_remained=n%m;
-            ans=i+1;
-            no_of_boxes=(n/m);
 
-        }
-    }
-    cout<<ans<<" "<<no_of_boxes;
-}
-*/
