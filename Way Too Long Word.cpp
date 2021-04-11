94f665226e049c84df439b884fa16c1c8111111c
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-04-11 13:28:06 +0530

    Create Way Too Long Word.cpp

---
+++ Way Too Long Word.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define ld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+
+int main()
+{
+    fastio;
+    test(){
+    string s;
+    cin>>s;
+    lli n=s.length();
+    if(n<=10)
+        cout<<s<<endl;
+    else
+        cout<<s[0]<<n-2<<s[n-1]<<endl;
+    }
+    return 0;
+}
