29ed25c1e74e1a4cbe73e745a6ac19141d0a59c1
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-04-11 12:28:42 +0530

    if(!strchr("AEIOUYaeiouy",s))->p(char)tolower(a);

---
+++ String Task.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define ld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+
+int main()
+{
+    fastio;
+    char s;
+    while(cin>>s)
+    {
+        s=tolower(s);
+        if(!(s=='a' || s=='e' || s=='i' || s=='y' || s=='o' || s=='u'))
+            cout<<"."<<s;
+    }
+}
