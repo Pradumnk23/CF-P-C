b1e6d47c6ea49bfd8ff2c38f256108c2525c31e7
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-04-16 20:00:53 +0530

    Create Nearly Lucky Number.cpp

---
+++ Nearly Lucky Number.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define ld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+
+int gcd(int A, int B)
+{
+    if (B == 0)
+        return A;
+    else
+        return gcd(B, A % B);
+}
+
+int main()
+{
+    fastio;
+    lli n,rem,cnt=0;
+    cin>>n;
+    while(n>0)
+    {
+        rem=n%10;
+        n/=10;
+        if(rem==4 || rem==7)
+            cnt++;
+    }
+    if(cnt==4 || cnt==7)
+        yes;
+    else
+        no;
+    return 0;
+}
